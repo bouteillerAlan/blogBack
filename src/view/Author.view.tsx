456f66fb43c92115deff8ad376d151567df8f366
Alan Bouteiller <bouteiller.alan@gmail.com>
2019-09-25 21:58:46 +0200

    :construction: feat (form) : add class for grid test

--- src/view/Author.view.tsx
+++ src/view/Author.view.tsx
@@ -2,28 +2,30 @@ import React from 'react';
 
 const authorView: React.FunctionComponent = () => {
     return (
-        <section>
-            <div className="input-block">
+        <section className="container row">
+            <div className="input-block col-xs_6">
                 <label htmlFor="firstName">First name </label>
                 <input type="text" name="firstName" id="firstName"/>
             </div>
 
-            <div className="input-block">
+            <div className="input-block col-xs_6">
                 <label htmlFor="lastName">Last name </label>
                 <input type="text" name="lastName" id="lastName"/>
             </div>
 
-            <div className="input-block">
+            <div className="input-block col-xs_12 col-l_12">
                 <label htmlFor="password">Password </label>
                 <input type="password" name="password" id="password"/>
             </div>
 
-            <div className="input-block">
+            <div className="input-block col-xs_12 col-l_12">
                 <label htmlFor="password">Message </label>
                 <textarea name="message" id="message"> </textarea>
             </div>
 
-            <button className="submit">Send</button>
+            <div className="col-xs_12 col-l_12">
+                <button className="submit">Send</button>
+            </div>
 
         </section>
     )
