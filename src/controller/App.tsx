028a69b50648dc21761b6c1167dc5ef9fe0f085a
Bouteiller <bouteiller.alan@gmail.com>
2019-09-25 13:20:52 +0200

    :sparkles: feat (routing) : add router

--- src/controller/App.tsx
+++ src/controller/App.tsx
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from '../style/image/logo.svg';
-import '../style/css/App.css';
+// vanilla
+import React, {Component} from 'react';
+// router
+import {Route, Router} from "react-router-dom";
+import { createBrowserHistory } from "history";
+// component
+import Author from "./Author";
+import Content from "./Content";
+import Cat from "./Categories";
+import NavbarView from "../view/Navbar.view";
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-};
+// history
+const history = createBrowserHistory();
+
+class App extends Component {
+    constructor(props: any) {
+      super(props);
+      this.state = {}
+    }
+
+    render() {
+        return (
+            <Router history={history}>
+                <NavbarView/>
+                <Route exact path='/' component={Content} />
+                <Route exact path='/categories' component={Cat} />
+                <Route exact path='/authors' component={Author} />
+            </Router>
+        )
+    }
+
+}
 
 export default App;
