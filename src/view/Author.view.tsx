bdf0c9b02c9e10d950873761cb8f0ab2544951e3
Bouteiller <bouteiller.alan@gmail.com>
2019-09-25 17:08:22 +0200

    :lipstick: style (input) : add dom input

--- src/view/Author.view.tsx
+++ src/view/Author.view.tsx
@@ -2,7 +2,30 @@ import React from 'react';
 
 const authorView: React.FunctionComponent = () => {
     return (
-        <p>Author</p>
+        <section>
+            <div className="input-block">
+                <label htmlFor="firstName">First name </label>
+                <input type="text" name="firstName" id="firstName"/>
+            </div>
+
+            <div className="input-block">
+                <label htmlFor="lastName">Last name </label>
+                <input type="text" name="lastName" id="lastName"/>
+            </div>
+
+            <div className="input-block">
+                <label htmlFor="password">Password </label>
+                <input type="password" name="password" id="password"/>
+            </div>
+
+            <div className="input-block">
+                <label htmlFor="password">Message </label>
+                <textarea name="message" id="message"> </textarea>
+            </div>
+
+            <button className="submit">Send</button>
+
+        </section>
     )
 };
 
