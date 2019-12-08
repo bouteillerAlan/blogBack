b6055a69cdd0296353f3011186f00260930aae1c
Alan Bouteiller <bouteiller.alan@gmail.com>
2019-12-08 14:06:05 +0100

    :sparkles: feat(login): check if user is already logged

--- src/controller/App.tsx
+++ src/controller/App.tsx
@@ -1,5 +1,5 @@
 // vanilla
-import React, {Component} from 'react';
+import React from 'react';
 // router
 import {Route, Router} from 'react-router-dom';
 import { createBrowserHistory } from 'history';
@@ -9,30 +9,54 @@ import Content from './Content';
 import Cat from './Categories';
 import NavbarView from '../view/Navbar.view';
 import Login from '../controller/Login';
-
+import handleLocalStorage from '../function/handleLocalStorage';
+import getData from '../function/getData';
 // history
 const history = createBrowserHistory();
 
 interface Props {}
-interface State {}
+interface State {
+  isLogged: boolean
+}
 
-class App extends Component {
-    constructor(props: any) {
-      super(props);
-      this.state = {}
+class App extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      isLogged: false
     }
+  }
 
-    render() {
-        return (
-          <Router history={history}>
-              <Route exact path='/' component={Login}/>
-              <NavbarView/>
-              <Route exact path='/content' component={Content} />
-              <Route exact path='/categories' component={Cat} />
-              <Route exact path='/authors' component={Author} />
-            </Router>
-        )
-    }
+  componentDidMount(): void {
+    handleLocalStorage('read','jwt')
+      .then((res: any) => {
+        if (res) {
+          getData('http://localhost:3001/auth/validate', res)
+            .then((res: any) => {
+              this.setState({isLogged: !!res.username});
+            })
+        }
+      })
+  }
+
+  render() {
+    const {isLogged} = this.state;
+    return (
+      <Router history={history}>
+        {!isLogged &&
+          <Route exact path='/' component={Login}/>
+        }
+        {isLogged &&
+          <>
+            <NavbarView/>
+            <Route exact path='/content' component={Content} />
+            <Route exact path='/categories' component={Cat} />
+            <Route exact path='/authors' component={Author} />
+          </>
+        }
+      </Router>
+    )
+  }
 
 }
 
